33c91a04c877f0681c50ca599a4472571188d331
bbha99 <branha99@gmail.com>
2023-01-19 17:37:09 -0800

    testing with asynchronization

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,25 +1,32 @@
 const request = require("request");
-let input = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${input}`, (error, response, body) => {
-  // console.log(typeof body);
-  // console.log(data)
-
-  // Edge Case: Request Failed
-  if (error) {
-    console.log(error.message)
-    return;
-  } 
-
-  // Edge Case: Breed Not Found
-  const data = JSON.parse(body);
-  const breed = data[0];
-  if (!breed) {
-    console.log("breed not found");
-    return
-  }
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    // console.log(typeof body);
+    // console.log(data)
   
-  console.log(breed.description); 
-
+    // Edge Case: Request Failed
+    if (error) {
+      // console.log(error.message)
+      callback(error.message, null);
+      return;
+    }
   
-});
+    // Edge Case: Breed Not Found
+    const data = JSON.parse(body);
+    const breed = data[0];
+    if (!breed) {
+      // console.log("breed not found");
+      callback("breed not found", null);
+      return;
+    }
+    // console.log(breed.description)
+    return callback(null, breed.description);
+  
+    
+  });
+};
+
+module.exports = {
+  fetchBreedDescription
+};
